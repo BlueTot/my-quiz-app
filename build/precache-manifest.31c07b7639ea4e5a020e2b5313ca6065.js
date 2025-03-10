b878f8fb51e76815b6005c4b5408cfd56f457369
BlueTot <bluetotkc@gmail.com>
2025-03-10 21:05:56 +0000

    added build

---
+++ build/precache-manifest.31c07b7639ea4e5a020e2b5313ca6065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dd90cb2f7e40e1452d69b98519fc5c",
+    "url": "/my-quiz-app/index.html"
+  },
+  {
+    "revision": "f861a1a0079953bf2687",
+    "url": "/my-quiz-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "96ec3fdb5490caf236e0",
+    "url": "/my-quiz-app/static/js/2.83544b1f.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/my-quiz-app/static/js/2.83544b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e1f6226b8a34d6319e",
+    "url": "/my-quiz-app/static/js/3.999629a8.chunk.js"
+  },
+  {
+    "revision": "f861a1a0079953bf2687",
+    "url": "/my-quiz-app/static/js/main.ab38a54e.chunk.js"
+  },
+  {
+    "revision": "553840a07459178ced0a",
+    "url": "/my-quiz-app/static/js/runtime-main.4c14320f.js"
+  }
+]);
